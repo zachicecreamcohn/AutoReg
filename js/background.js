77f55b6b696dec4382219dcf430a5a0ebed8194c
zachicecreamcohn <85762393+zachicecreamcohn@users.noreply.github.com>
2021-11-27 21:07:37 -0500

    initial commit

---
+++ js/background.js
@@ -0,0 +1,57 @@
+// this url will be compared to current tab. If they're equal, foreground.js is injected
+var reg_url = "https://acadinfo.wustl.edu/WSHome/Generic.aspx?Type=Registration&Page=/apps/Registration/"
+var reg_url_2 = "https://acadinfo.wustl.edu/WSHome/Generic.aspx?Type=Registration&Page=%2fapps%2fRegistration%2f"
+var reg_url_3 = "https://acadinfo.wustl.edu/WSHome/Generic.aspx?MenuID=67&Page=/apps/Registration/"
+var reg_url_4 = "https://talkbabytome.herokuapp.com/home"
+// var target_url = "https://www.saucedemo.com/inventory.html"
+
+var continue_to_run = true;
+
+        
+    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    // chrome.tabs.onActivated.addListener(tab => {
+        console.log("continue_to_run: " + continue_to_run)
+        if (continue_to_run == true) {
+        current_tab_info = tab
+            console.log(current_tab_info.url)
+            if (reg_url == current_tab_info.url || reg_url_2 == current_tab_info.url || reg_url_3 == current_tab_info.url || reg_url_4 == current_tab_info.url) {
+                chrome.tabs.insertCSS(null, {file: "./css/background.css"})
+
+                chrome.tabs.executeScript(null, {
+                    file: "./js/foreground.js"
+                },
+                    () => console.log("foreground.js injected"));
+                
+            } else {
+                console.log("foreground.js NOT injected. Not registration page.")
+            }
+        } else {
+            console.log("foreground.js NOT injected. continue_to_run is false.")
+        }
+    });
+
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log(request.reload);
+    if (request.reload == "true") {
+        console.log("background.js received message from foreground.js");
+        // reload the page
+        chrome.tabs.reload();
+    } else if (request.continue == "false") {
+        console.log("background.js received STOP message from foreground.js");
+        // stop running script on the page
+        alert("RSW1 Classes Have Been Added! Restart your browser to use the extension again.")
+        continue_to_run = false;
+    }
+    if (request.reg_page == "true") {
+        console.log("is reg page")
+    } else if (request.reg_page == "false") {
+        console.log("is NOT reg page")
+        continue_to_run = false;
+    }
+});
+   
+
+chrome.windows.onRemoved.addListener(function(windowId) {
+    continue_to_run = true;
+});
---
+++ js/foreground.js
@@ -0,0 +1,64 @@
+// document.querySelector('#contents_wrapper').classList.add('styletest')
+console.log('foreground.js loaded')
+
+var iframe = document.getElementById("ctl00_cphBody_genericIFrame");
+
+// if iframe exists:
+if (iframe) {
+    var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+    console.log("iframe found");
+    // send message to background.js that reg_page = true
+    chrome.runtime.sendMessage({"reg_page": "true"});
+} else {
+    console.log("iframe not found");
+    // send message to background.js that reg_page = false
+    chrome.runtime.sendMessage({"reg_page": "false"});
+}
+
+
+//get manual add first tab button
+var firstTabButton = iframeDoc.getElementById("__tab_ctl00_Body_tabRWS_tpnRWS1");
+console.log(firstTabButton);
+// if addCourseButton does not exist, then the page is not the add course page
+if (firstTabButton == null || firstTabButton == undefined){
+    console.log("First Tab Link Button not found");
+    // registration is closed
+    console.log("Registration is closed");
+    // chrome.runtime.sendMessage({
+    //     "reload": "true"
+    // });
+} else {
+    console.log("First Tab Link Button Found");
+    // registraion is open
+   // send message to background.js to reload the page
+
+// get the dropdown if class full select
+var dropdown = iframeDoc.getElementById("ctl00_Body_ddlFullCourseOption");
+// select "Waitlist Me from dropdown to prevent additional messages/steps if a class is full"
+dropdown.value = "add_to_waitlist";
+
+//select first choice tab
+
+    var firstChoiceTab = iframeDoc.getElementById("ctl00_Body_tabRWS_tpnRWS1"); //change to second choice "ctl00_Body_tabRWS_tpnRWS2"
+
+
+    //get array of courses in side of tab
+    var choices = firstChoiceTab.getElementsByClassName("lnkAddCourse");
+
+    // for i in choices, print to console
+    for (i = 0; i < choices.length; i++) {
+        
+        console.log("course" + i);
+        choices[i].click();
+    }
+
+    // send message to background.js to not reload the page
+    chrome.runtime.sendMessage({
+        "continue": "false"
+    });
+}
+
+function querySelectorIncludesText (doc, selector, text){
+    return Array.from(doc.querySelectorAll(selector))
+      .find(el => el.textContent.includes(text));
+  }
